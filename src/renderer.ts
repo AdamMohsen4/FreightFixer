6fdd0b0b1585f3201a86d993388784d51f015817
AdamMohsen4 <adam.amohsen4@gmail.com>
2025-04-04 20:53:48 +0200

    feat(ui): Add shipment simple GUI for program
    - Add package.json, .gitignore, tsconfig.json
    
    - Add index.html
    - Implement main.ts for
    Electron window setup
    
    - Add renderer.ts for UI interactions

---
+++ src/renderer.ts
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Get button elements
+    const settingsBtn = document.getElementById('settingsBtn');
+    const newShipmentBtn = document.getElementById('newShipmentBtn');
+    const viewShipmentsBtn = document.getElementById('viewShipmentsBtn');
+
+});
